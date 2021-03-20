989df50c5ae411864bbac8104236986445a32b16
TrevLee5 <tlee7@ggc.edu>
2021-03-20 18:55:11 -0400

    Changed variable names and added comments for clarity

--- src/Components/ImageGrid.js
+++ src/Components/ImageGrid.js
@@ -47,34 +47,35 @@ function Cards(props) {
     )
 }
 
+// returns rows of cards from database for Animals page
 const ImageGrid = () => {
     // get docs in images collection with useFirestore component
     const { docs } = useFirestore('Animals');
     console.log(docs);
 
-    // multiple rows
-    const grouped = docs.reduce((acc, post, ind) => {
-        var index = parseInt(ind / 4);
-        acc[index] = acc[index] || [];
-        acc[index].push(
+    // go through docs, build cards, and group cards by how long we want the row to be; accumulates an array of arrays, where each array in each index is 4 cards (for one row)
+    const groupedCards = docs.reduce((acc, currentValue, docsIndex) => {   // reduce method takes an accumulator, a current value, and an index and loops through docs
+        var accIndex = parseInt(docsIndex / 4); // docsIndex values of 0, 1, 2, and 3 will evalute to 0 here for our accumulator index (and then cards 4 through 7 will go in accIndex 1)
+        acc[accIndex] = acc[accIndex] || [];    // fallback value of empty array so that it will still render and avoid an error if we map over a null value
+        acc[accIndex].push( // build an animal card of the current animal in docs, and store in the current accumulator index (4 cards will go in each accIndex for a row)
             <div>
             <Cards 
                 // Sets the title to the animal in the card which is a sample animals for now 
-                title = {docs[ind].name}
+                title = {docs[docsIndex].name}
                 // sets the imag that will be shown on the card for the animal which iis a placeholder for now 
-                images = {docs[ind].url}
+                images = {docs[docsIndex].url}
                 // sets the description to the animal in the card which is a sample description for now 
                 description = "Sample text"
-                modalTitle = {docs[ind].name}
+                modalTitle = {docs[docsIndex].name}
                 modalInfo = "SECOND DESRC"
             />
             </div>
         );
-        return acc;
+        return acc; // the accumulator now holds multiple arrays: each with 4 cards
     }, []);
-    return (
+    return (    // map through the now-grouped cards, where each item is 4 cards (a row) and format each row, and return it to Animals.js
         <div>
-            {grouped.map((row, i) => {
+            {groupedCards.map((row, i) => {
                 return (
                     <motion.div className = "CardContainer"
                     initial={{ scale: 0 }}
@@ -85,53 +86,11 @@ const ImageGrid = () => {
                         damping: 20
                     }}>
                         <div className="row" key={`row-${i}`}>{row}</div>
-                        
                     </motion.div>
-                    
                     )
             })}
         </div>
     );
-
-    
-
-    //Keep formatting
-    // return (
-    //     <motion.div className = "CardContainer"
-    //      initial={{ scale: 0 }}
-    //      animate={{ rotate: 0, scale: 1 }}
-    //      transition={{
-    //        type: "spring",
-    //        stiffness: 260,
-    //        damping: 20
-    //      }}
-    //     >
-    //     <div className="row">
-    //         {/* make sure docs exist, cycle through and out template for each doc */}
-    //         { docs && docs.map((doc, i) => (
-                
-    //             <div>
-    //                 <Cards 
-    //                 // Sets the title to the animal in the card which is a sample animals for now 
-    //                 title = {doc.name}
-    //                 // sets the imag that will be shown on the card for the animal which iis a placeholder for now 
-    //                 images = {doc.url}
-    //                 // sets the description to the animal in the card which is a sample description for now 
-    //                 description = "Sample text"
-    //                 modalTitle = {doc.name}
-    //                 modalInfo = "SECOND DESRC"
-    //                 />
-                
-                
-
-    //                 {i++}
-    //                 {console.log(i)}
-    //             </div>
-    //         ))}
-    //     </div>
-        
-    //     </motion.div>
-    // )
 }
 
 export default ImageGrid;
