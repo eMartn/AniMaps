88e36d6c550ce01c2d5823f29d10d4a773ae5062
jgrimaldo404 <56837650+jgrimaldo404@users.noreply.github.com>
2021-03-23 23:51:34 -0400

    made it possible for different backgrounds in welcome & added 404 background

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styled from 'styled-components'
 import Image from '../Sprites/gecko.gif';
+import Image2 from '../Sprites/legs.gif';
+import Image3 from '../Sprites/elephant.gif';
+import Image4 from '../Sprites/rhino.gif';
+import Image5 from '../Sprites/cheetah.gif';
+import Image6 from '../Sprites/frog.gif';
+
 import {motion} from 'framer-motion';
 import {flashAnimation} from '../Animations';
 import { Button } from './Button';
@@ -11,33 +17,67 @@ import NavBar from '../Components/NavBar';
 // CSS for GIF
 // creates a const for section which is used below
 const Section = styled.section`
- background: url(${Image}) center;
+ //background: url(${Image}) center;
 height: 100%;
 height: 100vh;
-background-size: cover;
+//background-size: cover;
 display: flex;
 justify-content: center;
-background-repeat: no-repeat;
+//background-repeat: no-repeat;
 margin-top: -80px;
-opacity: 0.9;
+opacity: 0.8;
 background-filter: blur[5px];
+
+animation-name: animate;
+animation-direction: alterate-reverse;
+animation-duration: 30s;
+animation-fill-mode: forwards;
+animation-iteration-count: infinite;
+animation-play-state: running;
+animation-timing-function: ease-in-out;
+
+
+@keyframes animate{
+    
+0%{
+    background: url(${Image2}) center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+20%{
+    background: url(${Image6}) center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+40%{
+    background: url(${Image3}) center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+60%{
+    background: url(${Image4}) center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+80%{
+    background: url(${Image5}) center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+100%{
+    background: url(${Image}) center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+}
 `;
 
 // CSS for container which contains the p tag & button
 // creates a const for container which is used below
 const Container = styled.div`
-// Ignore this but it is old styling; Incase I need it
-// color: white;
-// padding: 1em;
-// margin: 20rem;
-// border-radius: 20px;
-// margin-left: 450px;
-// margin-right: 450px;
-// margin-left: 650vw;
-// margin-right: 650vw;
 // CSS for button
 button {    
-    text-decoration-color: blue;
+    text-decoration-color: green;
     margin-top: 25vh;
     font-size: clamp(2rem, 4vw, 2.15rem);
    padding: 2rem 5rem;
@@ -57,9 +97,9 @@ button {
 
 // CSS for paragraph
 p{
-font-size: clamp(1rem, 6vw, 1.2rem);
-color: whitesmoke;
--webkit-text-stroke-width: 0.6px;
+font-size: clamp(1.3rem, 6vw, 1.55rem);
+color: white;
+-webkit-text-stroke-width: 1.2px;
 -webkit-text-stroke-color: black;
 margin-top: 2vh;
 width: 750px;
