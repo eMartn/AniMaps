9dc1655154c98b770745bdedc2ebf5db3a055d9d
milkems <emartn97@gmail.com>
2021-04-06 23:49:32 -0400

    formatting

--- src/RenderProfile.js
+++ src/RenderProfile.js
@@ -102,34 +102,32 @@
                        </div>
                    </div>
               </div>                                           
-          ))}  
-           
-       </div>
-      );
-      }else if (output === "description")
-        {
-          let description;
-          {animals.map((animal) =>(             
-              description = animal.description
-              .replaceAll("\\n", "\n")                        
-          ))}  
+              ))}             
+          </div>
+          );
+        }else if (output === "description")
+          {
+            let description;
+            {animals.map((animal) =>(             
+                description = animal.description
+                .replaceAll("\\n", "\n")                        
+            ))}  
+              
+            return (
+                <div>
+                  
+                  {animals.map((animal) =>(             
+                      <div key={animal.id}>
+                        <p style={{textAlign: 'justified', whiteSpace: 'pre-wrap', width: '100%'}}>
+                        {description}</p>
             
-          return (
-              <div>
-                
-                {animals.map((animal) =>(             
-                    <div key={animal.id}>
-                      <p style={{textAlign: 'justified', whiteSpace: 'pre-wrap', width: '100%'}}>
-                      {description}</p>
-           
-                    </div>                            
-                ))}  
-            
-              </div>
-            )
-        }
-        
-      
+                      </div>                            
+                  ))}  
+              
+                </div>
+              )
+          }
+             
  }   
  
  export default AnimalProfileDeets;
